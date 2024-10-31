12eb28d10d37904f72f49648df29f52613b5139f
Nipuna Madula <nipunamadula2001@gmail.com>
2024-10-31 14:06:49 +0530

    Enhance Login component with header and input icon integration

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -6,6 +6,23 @@ import password_icon from '../Assets/Password.jpg'
 
 export const Login = () => {
   return (
-    <div>Login</div>
+    <div className='container'>
+    <div className='header'> 
+    <div className='text'>Login</div>
+    <div className='underline'></div>
+    </div>
+    <div className='inputs'>
+    <div className='input'>
+      <img src={user_icon} alt="" />
+      <input type="text" />
+    </div>
+      <div className='input'>
+      <img src={password_icon} alt="" />
+      <input type="password" />
+    </div>
+    </div>
+    </div>
   )
 }
+
+export default Login
