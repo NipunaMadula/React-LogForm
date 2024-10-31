4b6e59ea1b4fb74ba640a94cee5e91b72852cb45
Nipuna Madula <nipunamadula2001@gmail.com>
2024-10-31 13:32:46 +0530

    Update Login component to include user and password icons

---
+++ src/Components/Login/Login.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Login.css'
+
+import user_icon from '../Assets/Username.jpg'
+import password_icon from '../Assets/Password.jpg'
+
+export const Login = () => {
+  return (
+    <div>Login</div>
+  )
+}
